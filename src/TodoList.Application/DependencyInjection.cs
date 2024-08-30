d9b58240935e93b9042485ff2f8eb9a4dbca0e29
Bryan Kevin Condor Romero <vaayroon@outlook.com>
2024-08-30 13:39:21 +0200

    🔧 🎨 Refactor DependencyInjection.cs to add TodoList.Application.Core.Abstractions.Behaviors
    Add pipelines behaviors to DI and register all validator from assembly.

--- src/TodoList.Application/DependencyInjection.cs
+++ src/TodoList.Application/DependencyInjection.cs
@@ -2,6 +2,7 @@ using System.Reflection;
 using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using TodoList.Application.Core.Abstractions.Behaviors;
 
 namespace TodoList.Application;
 
@@ -9,28 +10,9 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        services.AddValidatorsFromAssembly(
-            typeof(DependencyInjection).Assembly,
-            includeInternalTypes: true);
-
-        return services;
-    }
-}
-
-/* ﻿using ApiService.Application.Core.Abstractions.Behaviors;
-using FluentValidation;
-using Microsoft.Extensions.DependencyInjection;
-
-namespace ApiService.Application;
-
-public static class DependencyInjection
-{
-    public static IServiceCollection AddApplication(this IServiceCollection services)
-    {
-        services.AddMediatR(config =>
+        services.AddMediatR(config => 
         {
             config.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
-            config.AddOpenBehavior(typeof(ExceptionHandlingPipelineBehavior<,>));
             config.AddOpenBehavior(typeof(RequestLoggingPipelineBehavior<,>));
             config.AddOpenBehavior(typeof(ValidationPipelineBehavior<,>));
         });
@@ -41,4 +23,4 @@ public static class DependencyInjection
 
         return services;
     }
-} */
+}
