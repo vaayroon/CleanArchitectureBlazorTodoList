4700bb3465a536ee4360b69b021f291e58dce214
Bryan Kevin Condor Romero <vaayroon@outlook.com>
2024-08-30 13:20:02 +0200

    🎨 Add Error and Success record types to TodoList.Domain.SharedKernel.Primitives

---
+++ src/TodoList.Domain/SharedKernel/Primitives/Error.cs
@@ -0,0 +1,56 @@
+﻿using TodoList.Domain.SharedKernel.Constants;
+
+namespace TodoList.Domain.SharedKernel.Primitives;
+
+public sealed record Error
+{
+    public static readonly Error None = new(string.Empty, string.Empty, ErrorType.Failure);
+    public static readonly Error NullValue = new(
+        "Error.NullValue",
+        "The specified result value is null.",
+        ErrorType.Failure);
+
+    public Error(string code, string message, ErrorType type)
+    {
+        Code = code;
+        Message = message;
+        Type = type;
+    }
+
+    public string Code { get; }
+
+    public string Message { get; }
+
+    public ErrorType Type { get; }
+
+    public static implicit operator string(Error error) => error.Code;
+
+    public static implicit operator Result(Error error) => Result.Failure(error);
+
+    public static Error Failure(string code, string message) =>
+        new(code, message, ErrorType.Failure);
+
+    public static Error NotFound(string code, string message) =>
+        new(code, message, ErrorType.NotFound);
+
+    public static Error Problem(string code, string message) =>
+        new(code, message, ErrorType.Problem);
+
+    public static Error Conflict(string code, string message) =>
+        new(code, message, ErrorType.Conflict);
+
+    public bool Equals(Error? other)
+    {
+        if (other is null)
+        {
+            return false;
+        }
+
+        return Code == other.Code && Message == other.Message;
+    }
+
+    public override int GetHashCode()
+    {
+        return Code.GetHashCode(StringComparison.InvariantCultureIgnoreCase);
+    }
+}
---
+++ src/TodoList.Domain/SharedKernel/Primitives/Success.cs
@@ -0,0 +1,39 @@
+﻿namespace TodoList.Domain.SharedKernel.Primitives;
+
+public sealed record Success
+{
+    public static readonly Success None = new(string.Empty, string.Empty);
+    public static readonly Success NullValue = new(
+        "Success.NullValue",
+        "The specified result value is null.");
+
+    public Success(string code, string message)
+    {
+        Code = code;
+        Message = message;
+    }
+
+    public string Code { get; }
+
+    public string Message { get; }
+
+    public static implicit operator string(Success success) => success.Code;
+
+    public static implicit operator Result(Success success) => Result.Successs(success);
+
+    public bool Equals(Success? other)
+    {
+        if (other is null)
+        {
+            return false;
+        }
+
+        return Code == other.Code && Message == other.Message;
+    }
+
+    public override int GetHashCode()
+    {
+        return Code.GetHashCode(StringComparison.InvariantCultureIgnoreCase);
+    }
+}
+
